e96ddbdc7d169ee1fce997de60c8b708db110176
h3thal <h3thal@yandex.ru>
2021-08-23 00:41:27 +0300

    git init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="circle"></div>
+  <Navbar/>
+  <Home/>
+</template>
+
+<script>
+import Navbar from './components/Nav/Navbar.vue'
+import Home from './components/Home/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    Home
+  }
+}
+</script>
+
+<style lang="scss">
+  #circle {
+    position: absolute;
+    width: 766px;
+    height: 766px;
+    left: -149px;
+    top: -147px;
+    background: #FF523D;
+    opacity: 0.05;
+    filter: blur(150px);
+    z-index: -5;
+  }
+</style>
---
+++ src/components/Home/Home.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="home">
+    <div class="wrapper">
+      <div class="home__block">
+        <div class="home__block-title">
+          The best video online meeting software.
+        </div>
+        <div class="home__block-subtitle">
+          Many desktop publishing packages and web page editors now use Lorem Ipsum as their default
+        </div>
+        <div class="home__block-button">
+          Start for free
+        </div>
+      </div>
+      <img src="@/assets/home.png" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/__extends.scss';
+#home {
+  height: 520px;
+  .home__block {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 50%;
+    &-title {
+      color: $navyBlue;
+      font-family: Inter;
+      font-style: normal;
+      font-weight: 600;
+      font-size: 48px;
+      line-height: 60px;
+      width: 498px;
+    }
+    &-subtitle {
+      color: $navyBlue;
+      font-family: Inter;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 17px;
+      line-height: 28px;
+      width: 484px;
+    }
+    &-button {
+      color: $navyBlue;
+      font-family: Inter;
+      font-style: normal;
+      font-weight: 500;
+      font-size: 17px;
+      line-height: 21px;
+      background-color: $neonBlue;
+      width: 180px;
+      height: 66px;
+      border-radius: 6px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      cursor: pointer;
+    }
+  }
+}
+</style>
